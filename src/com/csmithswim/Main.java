1909f2209aa18036c89d35ec9b042c4d6d285d0f
Cullen S <cullensmi@gmail.com>
2020-12-06 18:43:21 -0500

    Solved algorithm

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -13,7 +13,14 @@ public class Main {
 
     public static void main(String[] args) {
         int n = scanner.nextInt();
+        String[] loopCount = String.valueOf(Integer.toBinaryString(n)).trim().split("0");
+        int count=0;
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        Arrays.sort(loopCount);
+        System.out.println(loopCount[loopCount.length-1].length());
+
+//        System.out.println(Arrays.toString(loopCount));
+//        System.out.println(Integer.toBinaryString(n));
 
         scanner.close();
     }
